4322d46a400f8c2d1ad9e09021e928ae81021c00
kassio wl <kassiowl34@gmail.com>
2022-06-09 15:18:56 -0300

    saida.csv add

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ const fs = require("fs");
 const json2csv = require('json2csv').parse;
 var csv = require('csv-parser');
 
-
-const geocoder = NodeGeocoder(options);
-
 function getDistanceFromLatLonInKm(position1, position2) {          //calcular distancia de 2 posicoes
     "use strict";
     var deg2rad = function (deg) { return deg * (Math.PI / 180); },
@@ -19,6 +16,44 @@ function getDistanceFromLatLonInKm(position1, position2) {          //calcular d
     return ((R * c *1000).toFixed());
 }
 
+var ConverterEstados = function(val) {
+	var data;
+
+	switch (val.toUpperCase()) {
+
+		
+		/* Estados */
+		case "ACRE" :					data = "AC";	break;
+		case "ALAGOAS" :				data = "AL";	break;
+		case "AMAZONAS" :				data = "AM";	break;
+		case "AMAPÁ" :					data = "AP";	break;
+		case "BAHIA" :					data = "BA";	break;
+		case "CEARÁ" :					data = "CE";	break;
+		case "DISTRITO FEDERAL" :		data = "DF";	break;
+		case "ESPÍRITO SANTO" :			data = "ES";	break;
+		case "GOIÁS" :					data = "GO";	break;
+		case "MARANHÃO" :				data = "MA";	break;
+		case "MINAS GERAIS" :			data = "MG";	break;
+		case "MATO GROSSO DO SUL" :		data = "MS";	break;
+		case "MATO GROSSO" :			data = "MT";	break;
+		case "PARÁ" :					data = "PA";	break;
+		case "PARAÍBA" :				data = "PB";	break;
+		case "PERNAMBUCO" :				data = "PE";	break;
+		case "PIAUÍ" :					data = "PI";	break;
+		case "PARANÁ" :					data = "PR";	break;
+		case "RIO DE JANEIRO" :			data = "RJ";	break;
+		case "RIO GRANDE DO NORTE" :	data = "RN";	break;
+		case "RONDÔNIA" : 				data = "RO";	break;
+		case "RORAIMA" :				data = "RR";	break;
+		case "RIO GRANDE DO SUL" :		data = "RS";	break;
+		case "SANTA CATARINA" :			data = "SC";	break;
+		case "SERGIPE" :				data = "SE";	break;
+		case "SÃO PAULO" :				data = "SP";	break;
+		case "TOCANTÍNS" :				data = "TO";	break;
+	}
+    return data
+}
+//creditos https://gist.github.com/renancouto/3784690
 
 
 
@@ -51,15 +86,13 @@ fs.createReadStream('empresas_bahia_lista_3.csv')
   .on('data', function (data) 
   {
 
-      var address = data.cep
+    var address = data.cep
 
-      const letdataLatLngObj = geocoder.geocode(data.cep);
-
-
-    data.dist_1 = getDistanceFromLatLonInKm(salvador,letdataLatLngObj)    //arrumar um jeito de pegar a latitude e longetude de cada empresa
-    data.dist_2 = getDistanceFromLatLonInKm(feira_santana,letdataLatLngObj)
-    data.dist_3 = getDistanceFromLatLonInKm(vitoria_conquista,letdataLatLngObj)
-    data.dist_4 = getDistanceFromLatLonInKm(camaçari,letdataLatLngObj)
+    data.dist_1 = ''  
+    data.dist_2 = ''
+    data.dist_3 = ''
+    data.dist_4 = ''
+    dataArray.push(data)
  
   })
   .on("end", function()
@@ -67,7 +100,102 @@ fs.createReadStream('empresas_bahia_lista_3.csv')
     console.log(dataArray[0])
     var result = json2csv(dataArray);
     console.log(result[0])
-    fs.writeFileSync('empresas_bahia_lista_3v2.csv', result);
+    fs.writeFileSync('empresas_bahia_lista_3.csv', result);
+    dataArray = []
+  })
+  .on("error", function(error)
+  {
+    console.log(error)
+  })
+
+fs.createReadStream('empresas_bahia_lista_3.csv')
+  .pipe(csv())
+  .on('data', function (data) 
+  {
+
+    data.dist_1 = ''  
+    data.dist_2 = ''
+    data.dist_3 = ''
+    data.dist_4 = ''
+    dataArray.push(data)
+ 
+  })
+  .on("end", function()
+  {
+    console.log(dataArray[0])
+    var result = json2csv(dataArray);
+    console.log(result[0])
+  })
+  .on("error", function(error)
+  {
+    console.log(error)
+  })
+
+fs.createReadStream('cidade_siafi.csv')
+  .pipe(csv())
+  .on('data', function (data) 
+  {
+    delete data.codigo_uf;
+    delete data.siafi_id
+    delete data.nome;
+    delete data.codigo_ibge;
+    data.latitude_cidade = data.latitude
+    data.longitude_cidade = data.longitude
+    data.nome_fantasia = null
+    data.slug = null
+    data.inicio_atividades = null
+    data.porte_empresa = null
+    data.nome_cidade = null
+    data.sigla_uf= null
+    data.populacao_cidade = null
+    data.dist_1 = null  //arrumar um jeito de pegar a latitude e longetude de cada empresa
+    data.dist_2 = null
+    data.dist_3 = null
+    data.dist_4 = null
+    delete data.latitude
+    delete data.longitude
+    dataArray.push(data)
+  })
+  .on("end", function()
+  {
+    console.log(dataArray[0])
+    var result = json2csv(dataArray);
+    console.log(result[0])
+  })
+  .on("error", function(error)
+  {
+    console.log(error)
+  })
+
+fs.createReadStream('cidade_populacao.csv')
+  .pipe(csv())
+  .on('data', function (data) 
+  {
+    data.populacao_cidade = data.populacao;
+    data.sigla_uf = ConverterEstados(data.nome_uf);
+    data.nome_fantasia = null
+    data.slug = null
+    data.inicio_atividades  = null
+    data.porte_empresa = null
+    data.nome_cidade = null
+    data.latitude_cidade = null
+    data.longitude_cidade= null
+    data.dist_1 = null //arrumar um jeito de pegar a latitude e longetude de cada empresa
+    data.dist_2 = null
+    data.dist_3 = null
+    data.dist_4 = null
+    delete data.nome_uf;
+    delete data.populacao;
+    delete data.cod_ibge;
+    delete (data['﻿cod_ibge']);
+    dataArray.push(data)
+  })
+  .on("end", function()
+  {
+    console.log(dataArray[0])
+    var result = json2csv(dataArray);
+    console.log(result[0])
+    fs.writeFileSync('saida.csv', result);
   })
   .on("error", function(error)
   {
